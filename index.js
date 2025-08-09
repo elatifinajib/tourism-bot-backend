dbe0ea422d83905448eebbea13f96bed516a115b
El-Atifi Najib <elatifinajib@gmail.com>
2025-08-09 21:26:06 +0100

    Simplification backend - Only Ask_All_Attractions intent6

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.post('/webhook', async (req, res) => {
         return res.json({ fulfillmentText: "I couldn't find any attractions for you." });
       }
 
-      // On liste toutes les attractions avec le format souhaité
-      const list = attractions.map(a => `- ${a.name} (${a.cityName})`).join('\n');
-      const reply = `Here are the attractions:\n${list}`;
+      // On liste toutes les attractions avec un retour à la ligne HTML <br> entre chaque
+      const list = attractions.map(a => `- ${a.name} (${a.cityName})`).join('<br>');
+      const reply = `Here are the attractions:<br>${list}`;
 
       return res.json({
         fulfillmentText: reply,
